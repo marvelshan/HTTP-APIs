3ad5bf66684e2c0de0a6ae33d4cf841ad2965e77
zaki <reborn7875@gmail.com>
2024-03-14 21:07:08 +0800

    test: test sign up API

---
+++ tests/test_create_auth.py
@@ -0,0 +1,83 @@
+import pytest
+from app import app
+from json import dumps
+
+
+@pytest.fixture
+def client():
+    """Creates a Flask test client with TESTING mode enabled."""
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_signUp_request_valid_data(client):
+    """Tests the /signUp endpoint with valid user data."""
+    response = client.post(
+        "/api/signUp",
+        data=dumps(
+            {
+                "username": "Flask25",
+                "password": "testPassword1234",
+            }
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 201
+    assert response.json["success"] is True
+
+
+def test_signUp_request_username_exists(client):
+    """Tests the /signUp endpoint with valid user data."""
+    response = client.post(
+        "/api/signUp",
+        data=dumps(
+            {
+                "username": "Flask20",
+                "password": "testPassword1234",
+            }
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+    assert response.json["success"] is False
+    assert response.json["reason"] == "Username already exists"
+
+
+def test_signUp_request_valueError_lack_Uppercase(client):
+    """Tests the /signUp endpoint with valid user data."""
+    response = client.post(
+        "/api/signUp",
+        data=dumps(
+            {
+                "username": "Flask1",
+                "password": "testpassword1234",
+            }
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+    assert response.json["success"] is False
+    assert (
+        response.json["reason"]
+        == "Value error, Password must contain at least one uppercase letter"
+    )
+
+
+def test_signUp_request_valueError_lack_Uppercase(client):
+    """Tests the /signUp endpoint with valid user data."""
+    response = client.post(
+        "/api/signUp",
+        data=dumps(
+            {
+                "username": "Flask24",
+                "password": "testpassword1234",
+            }
+        ),
+        content_type="application/json",
+    )
+    assert response.status_code == 400
+    assert response.json["success"] is False
+    assert response.json["reason"] == [
+        "Value error, Password must contain at least one uppercase letter"
+    ]
