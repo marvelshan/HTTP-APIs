92b4f05bb429d100a5e6f68e17c6fcbfbc37c23d
zaki <reborn7875@gmail.com>
2024-03-15 14:03:18 +0800

    docs: add Swagger YAML file and docstrings

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flasgger import Swagger
 from blueprints.authentication import authentication_bp
 from models.database import db
 
 load_dotenv()
 
 app = Flask(__name__)
+swagger = Swagger(app, template_file="docs/swagger.yml")
+
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -15,6 +18,13 @@ db.init_app(app)
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """
+    Handle 404 errors by returning a custom JSON response.
+    Parameters:
+        - error: The error object provided by Flask.
+    Returns:
+        - A tuple containing a Flask jsonify response and the HTTP status code.
+    """
     error_message = {
         "message": "This page does not exist", "error": str(error)}
     return jsonify(error_message), 404
@@ -22,6 +32,13 @@ def page_not_found(error):
 
 @app.errorhandler(Exception)
 def handle_exception(error):
+    """
+    Handle general exceptions by returning a custom JSON response.
+    Parameters:
+        - error: The error object provided by Flask.
+    Returns:
+        - On unexpected errors, returns a JSON object with an error message and a 500 status code.
+    """
     error_message = {"message": "An error occurred", "error": str(error)}
     return jsonify(error_message), 500
 
@@ -30,4 +47,4 @@ app.register_blueprint(authentication_bp, url_prefix="/api")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
--- blueprints/authentication.py
+++ blueprints/authentication.py
@@ -11,6 +11,18 @@ authentication_bp = Blueprint("authentication_bp", __name__)
 
 @authentication_bp.route("/signUp", methods=["POST"])
 def sign_up():
+    """
+    Register a new user.
+
+    This endpoint receives a JSON request containing the user's username and password.
+    The password is hashed using bcrypt before being stored in the database.
+
+    Returns:
+        - A JSON response indicating success or failure.
+            - If successful, returns {"success": True} and status code 201.
+            - If there are validation errors, returns {"success": False, "reason": [error messages]} and status code 400.
+            - If a user with the same username already exists, returns {"success": False, "reason": "Username already exists"} and status code 409.
+    """
     try:
         Sign_up_input(**request.json)
         data = request.json
@@ -35,11 +47,24 @@ def sign_up():
     except IntegrityError:
         db.session.rollback()
         error_message = {"success": False, "reason": "Username already exists"}
-        return jsonify(error_message), 400
+        return jsonify(error_message), 409
 
 
 @authentication_bp.route("/signIn", methods=["POST"])
 def sign_in():
+    """
+    Sign in an existing user.
+
+    This endpoint receives a JSON request containing the user's username and password.
+    It checks if the username exists in the database and if the provided password matches the hashed password.
+
+    Returns:
+        - A JSON response indicating success or failure.
+            - If successful, returns {"success": True} and status code 200.
+            - If the username or password is invalid, returns {"success": False, "reason": "Invalid password"} and status code 401.
+            - If the user does not exist, returns {"success": False, "reason": "User not found"} and status code 404.
+        - If there are validation errors, returns {"success": False, "reason": [error messages]} and status code 400.
+    """
     try:
         Sign_in_input(**request.json)
         data = request.json
